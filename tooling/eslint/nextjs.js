966fe64553c9a231c2af9792625cba527dcab5e9
xuatz <xzlow10@gmail.com>
2025-06-20 23:04:50 +0900

    wip

--- tooling/eslint/nextjs.js
+++ tooling/eslint/nextjs.js
@@ -1,10 +1,24 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  extends: ["plugin:@next/next/core-web-vitals"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "@typescript-eslint/require-await": "off",
-  },
-};
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 
-module.exports = config;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends("plugin:@next/next/core-web-vitals"),
+
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/require-await": "off",
+    },
+  },
+]);
--- tooling/eslint/react.js
+++ tooling/eslint/react.js
@@ -1,27 +1,47 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-  ],
-  rules: {
-    "react/prop-types": "off",
-    "jsx-a11y/no-autofocus": "off",
-    "jsx-a11y/label-has-associated-control": "off",
-    "react-hooks/exhaustive-deps": "off",
-  },
-  globals: {
-    React: "writable",
-  },
-  settings: {
-    react: {
-      version: "detect",
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended",
+      ),
+    ),
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        React: "writable",
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "react/prop-types": "off",
+      "jsx-a11y/no-autofocus": "off",
+      "jsx-a11y/label-has-associated-control": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   },
-  env: {
-    browser: true,
-  },
-};
-
-module.exports = config;
+]);
